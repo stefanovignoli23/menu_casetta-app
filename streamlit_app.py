8e6ff6690a9670529c429fdd0ce9f9a70e14f13b
stefanovignoli23 <stefano.vignoli23@gmail.com>
2026-02-03 10:46:29 +0000

    added config and other mods

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,66 +3,70 @@ import pandas as pd
 import numpy as np
 from streamlit_gsheets import GSheetsConnection
 
-#@st.cache_data
+@st.cache_data
+def load_data():
+    conn = st.connection("gsheets",type = GSheetsConnection)
+    df = conn.read(spreadsheet=st.secrets['spreadsheet'])
+    return df
 
-conn = st.connection("gsheets",type = GSheetsConnection)
-df = conn.read(spreadsheet=st.secrets['spreadsheet'])
+if st.sidebar.button("🔄 Aggiorna il menu"):
+    load_data.clear()
+    st.rerun()
+
+df = load_data()
 df["ingredienti"] = df["ingredienti"].apply(
     lambda x: [i.strip() for i in x.split(",")]
 )
 df = df[df.disponibile=='S']
-df
-df['categoria'] = np.where(df.categoria=='C','Caffè',
-                np.where(df.categoria=='T','Tisane e Tè',
-                np.where(df.categoria=='A','Amari',
-                np.where(df.categoria=='B','Bevande fredde',
-                df.categoria))))
 
 
 st.title("☕ Menu Caffetteria")
 
-ingrediente_filtro = st.text_input(
-    "🔍 Filtra per ingrediente (es. zenzero)"
+lista_ingredienti_unici = df["ingredienti"].explode().sort_values().unique().tolist()
+opzioni_ingredienti = st.multiselect(
+    "🔍 Ti piacerebbe sentire qualcosa in particolare?",
+    lista_ingredienti_unici,
+    placeholder="Scegli i tuoi ingredienti preferiti...",
 )
 senza_caffeina = st.toggle('Senza caffeina')
 
 df_filtrato = df.copy()
-
-if ingrediente_filtro:
+if opzioni_ingredienti:
     df_filtrato = df_filtrato[
         df_filtrato["ingredienti"].apply(
-            lambda ingr: ingrediente_filtro.lower() in [i.lower() for i in ingr]
+            lambda ingr: all(i in ingr for i in opzioni_ingredienti)
         )
     ]
 if senza_caffeina:
     df_filtrato = df_filtrato[df_filtrato["caffeina"]=='N']
 
-categorie = sorted(df["categoria"].unique())
+categorie = sorted(df_filtrato["categoria"].unique())
 
+try:
+    tabs = st.tabs(categorie)
+    for tab, categoria in zip(tabs, categorie):
+        with tab:
+            prodotti_categoria = df_filtrato[
+                df_filtrato["categoria"] == categoria
+            ]
 
-tabs = st.tabs(categorie)
+            if prodotti_categoria.empty:
+                st.info("Nessun prodotto disponibile")
+                continue
 
-for tab, categoria in zip(tabs, categorie):
-    with tab:
-        prodotti_categoria = df_filtrato[
-            df_filtrato["categoria"] == categoria
-        ]
+            prodotto_selezionato = st.radio(
+                "Seleziona un prodotto",
+                sorted(prodotti_categoria["nome"].tolist()),
+                label_visibility="collapsed"
+            )
 
-        if prodotti_categoria.empty:
-            st.info("Nessun prodotto disponibile")
-            continue
+            prodotto = prodotti_categoria[
+                prodotti_categoria["nome"] == prodotto_selezionato
+            ].iloc[0]
 
-        prodotto_selezionato = st.radio(
-            "Seleziona un prodotto",
-            prodotti_categoria["nome"].tolist(),
-            label_visibility="collapsed"
-        )
-
-        prodotto = prodotti_categoria[
-            prodotti_categoria["nome"] == prodotto_selezionato
-        ].iloc[0]
-
-        st.divider()
-        st.markdown(f"## {prodotto['nome']}")
-        st.write(f"**Descrizione:** {prodotto['descrizione']}")
-        st.write(f"**Ingredienti:** {', '.join(prodotto['ingredienti'])}")
+            st.divider()
+            st.markdown(f"## {prodotto['nome']}")
+            st.write(f"**Descrizione:** {prodotto['descrizione']}")
+            st.write(f"**Ingredienti:** {', '.join(prodotto['ingredienti'])}")
+except:
+    st.warning('Non ci sono prodotti disponibili con queste combinazioni di ingredienti! Ritenta :)', icon="⚠️")
